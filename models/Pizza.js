652571d7cd324001beda9a98fffb0c6693b41206
Alexy Kotliar <kotliar.alex@gmail.com>
2021-06-21 16:06:10 -0400

    added crud for comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,26 +1,46 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchmena = new Schema({
-
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String
+      type: String
     },
     createdBy: {
-        type:String
+      type: String
     },
-    createdAt:{
-        type:Date,
-        default:Date.now
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
     },
-    size:{
-        type:String,
-        default:'Large'
+    size: {
+      type: String,
+      default: 'Large'
     },
-    toppings:[]
-})
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
 
-//create the pizza model using the pizza schema 
-const Pizza = model('Pizza', PizzaSchmena);
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
 
-//export the Pizza model
+const Pizza = model('Pizza', PizzaSchema);
+
 module.exports = Pizza;
