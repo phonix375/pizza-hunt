ca20a1588764fe38ec6a14dbdc5632bd6956ca1c
Alexy Kotliar <kotliar.alex@gmail.com>
2021-07-03 10:46:36 -0400

    save point

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -13,7 +13,7 @@ const pizzaController = {
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.sendStatus(400).json(err);
+        res.sendStatus(400);
       });
   },
 
--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 const {
-    getAllPizza,
-    getPizzaById,
-    createPizza,
-    updatePizza,
-    deletePizza
-} = require('../../controllers/pizza-controller')
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
 
-//set up get all POST at /api/pizzas
-router.route('/')
-.get(getAllPizza)
-.post(createPizza);
-
-//Set up GET one, PUT, and DELETE at /api/pizzas/:id
+// /api/pizzas
 router
-    .route('/:id')
-    .get(getPizzaById)
-    .put(updatePizza)
-    .delete(deletePizza)
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
 
+// /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
 
-    module.exports = router;
+module.exports = router;
